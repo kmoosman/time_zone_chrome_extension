5d6fce4d23428c10ed4295438c6310e5996ea708
Katie Moosman <moosman.katie@gmail.com>
2019-03-19 11:57:32 -0700

    setting up git

--- popup.js
+++ popup.js
@@ -1,17 +1,66 @@
-function regCheck(){
-    chrome.tabs.executeScript(null, {file: "regCheck.js"});
-    };
+function time(zone) {
 
-function checkNesting(){
-    chrome.tabs.executeScript(null, {file: "regNesting.js"});
-    };
+    switch(zone) {
+        case 'est':
+            today = new Date().toLocaleTimeString("en-US", {hour: '2-digit', minute:'2-digit', timeZone: "America/New_York"})
+            return today
+          break;
+        case 'cst':
+            today = new Date().toLocaleTimeString("en-US", {hour: '2-digit', minute:'2-digit', timeZone: "America/Chicago"})
+            return today
+          break;
+        case 'pst':
+            today = new Date().toLocaleTimeString("en-US", {hour: '2-digit', minute:'2-digit', timeZone: "America/Los_Angeles"})
+            return today
+        case 'mst':
+            today = new Date().toLocaleTimeString("en-US", {hour: '2-digit', minute:'2-digit', timeZone: "America/Denver"})
+            return today
+        case 'jst':
+            tomorrow = new Date()
+            today = new Date().toLocaleString("en-US", {hour: '2-digit', minute:'2-digit',timeZone: 'Asia/Tokyo'})
+            day = tomorrow.getDay()
+            var textDay
+            console.log(textDay)
+            switch(day){
+                case 0:
+                    textDay = "Mon"
+                break;
+                case 1:
+                    textDay = "Tues"
+                break;
+                case 2:
+                    textDay = "Wed"
+                break;
+                case 3:
+                    textDay = "Thurs"
+                break;
+                case 4:
+                    textDay = "Fri"
+                break;
+                case 5:
+                    textDay = "Sat"
+                break;
+                case 6:
+                    textDay = "Sun"
+                break;
+                default:
+                    return "error"
+            }
 
-function goGoLink(){
-    chrome.tabs.executeScript(null, {file: "goGolink.js"});
-    window.close()
-    
-    };
+            console.log(textDay)
+               
 
-document.getElementById("regCheck").onclick = regCheck;
-document.getElementById("regNesting").onclick = checkNesting;
-document.getElementById("goGoLink").onclick = goGoLink;
+            return `${textDay} ${today}`
+        default:
+            return "error"
+
+      }
+}
+
+
+// document.getElementById("regCheck").onclick = regCheck;
+document.getElementById("cst").textContent = time('cst');
+document.getElementById("pst").textContent = time('pst');
+document.getElementById("japan").textContent = time('jst');
+document.getElementById("mst").textContent = time('mst');
+document.getElementById("est").textContent = time('est');
--- regCheck.js
+++
@@ -1,7 +0,0 @@
-var eid = document.location.href.substring(document.location.href.indexOf("uidEvent/") + 9)
-
-newWindow()
-      
-function newWindow(){
-    window.open('https://redash.motorsportreg.com/queries/180?p_uidEvent=' + eid + '&p_uidevent=' + eid + '#232')
-}
